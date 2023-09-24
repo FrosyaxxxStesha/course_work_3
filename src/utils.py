82b3a0578013f87234aa6e434593fadc25730e44
Tsurkan <mikh.tsurkan@gmail.com>
2023-09-24 19:03:11 +0300

    добавлены docstrings, все тесты пройдены, подготовка к мёрджу

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,10 @@ from json import load
 
 
 def filter_canceled(py_obj_list):
+    """
+    функция, фильтрующая операции по статусу, пропускает пустые словари,
+    возвращает отфильтрованный объект python
+    """
     without_canc = []
 
     for py_dict in py_obj_list:
@@ -18,16 +22,26 @@ def filter_canceled(py_obj_list):
 
 
 def drop_json(json_file):
+    """
+    Получает файл json, декодирует его в объект python,
+    возвращает объект python
+    """
     with open(json_file) as file:
         py_obj = load(file)
     return py_obj
 
 
 def parse_date(py_obj_dict):
+    """
+    извлекает дату из переданного словаря и возвращает её
+    """
     return py_obj_dict["date"]
 
 
 def take_date(date_str):
+    """
+    переводит строку даты в объект datetime
+    """
     year = int(date_str[:4])
     month = int(date_str[5:7])
     day = int(date_str[8:10])
@@ -39,12 +53,19 @@ def take_date(date_str):
 
 
 def take_operation(date, py_obj):
+    """
+    получает операцию по дате
+    """
     for operation in py_obj:
         if operation["date"] == date:
             return operation
 
 
 def format_operation(py_dict):
+    """
+    форматирует операцию для дальнейшего выовода,
+    возвращает кортеж данных
+    """
     date = py_dict["date"][8:10] + "." + py_dict["date"][5:7] + "." + py_dict["date"][:4]
     try:
         if "Счет" in py_dict["to"]:
