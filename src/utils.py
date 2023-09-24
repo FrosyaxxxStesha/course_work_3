544d3708332e34c598a7a176244d9d4b68853fbd
Tsurkan <mikh.tsurkan@gmail.com>
2023-09-24 11:33:56 +0300

    написаны первые тесты

---
+++ src/utils.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+
+def filter_canceled(py_obj_list):
+    pass
+
+
+def drop_json(json_file):
+    pass
+
+
+def parse_date(py_obj_dict):
+    pass
+
+
+def take_date(date_str):
+    pass
+
+
+def take_operation(date, py_obj):
+    pass
+
+
+def format_operation(py_dict):
+    pass
---
+++ tests/test_utils.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+import json
+from src import utils
+
+with open("../operations.json") as file:
+    py_obj = json.load(file)
+
+
+def test_filter_canceled():
+    utils.filter_canceled(py_obj)
+    flag_cancelled = False
+    for i in py_obj:
+        if i["state"] != "EXECUTED":
+            flag_cancelled = True
+            break
+
+    assert not flag_cancelled
+
+
+def test_drop_json():
+    assert type(utils.drop_json("operations.json")) == list
+
+
+def test_parse_date():
+    for i in py_obj:
+        assert utils.parse_date(i) == i["date"]
+
+
+def test_take_date():
+    assert utils.take_date("2017-06-24T10:50:58.294041") == datetime(2017, 6, 24, 10, 50, 58, 294041)
+
+
+def test_take_operation():
+    for i in py_obj:
+        assert utils.take_operation(i["date"], py_obj) == i
+
+
+def test_format_operation():
+    for i in py_obj:
+        date = i["date"][8:10] + "." + i["date"][5:7] + "." + i["date"][:4]
+        try:
+            if "Счет" in i["to"]:
+                from_op = "Счет " + "**" + i["to"][-4:]
+            else:
+                from_op = i["to"][:-16] + i["to"][-16: -12] + " " + i["to"][-12:-10] + "** **** " + i["to"][-4:]
+        except KeyError:
+            from_op = "?"
+        if "Счет" in i["to"]:
+            to = "Счет " + "**" + i["to"][-4:]
+        else:
+            to = i["to"][:-16] + i["to"][-16: -12] + " " + i["to"][-12:-10] + "** **** " + i["to"][-4:]
+        assert utils.format_operation(i) == (
+                                             date,
+                                             i["description"],
+                                             from_op,
+                                             to,
+                                             i["operationAmount"]["amount"],
+                                             i["operationAmount"]["currency"]["name"]
+                                             )
